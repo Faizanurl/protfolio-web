75657f1cfd687d4b0a5a1a89aa8618651363228f
Faizandevsinn <faizandevsinn3@gmail.com>
2025-08-30 22:21:05 +0500

    add some things

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3224389665"
+    serviceWorkerVersion: "2909194431"
   }
 });
